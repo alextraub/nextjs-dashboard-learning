efb148816d1102491380263f62cdcfa39dae6c19
Alex Traub <atraub123@gmail.com>
2023-11-24 12:11:27 -0600

    feat: auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
 
 export type State = {
   errors?: {
@@ -109,4 +110,18 @@ export async function deleteInvoice(id: string) {
 
   revalidatePath('/dashboard/invoices');
   return { message: 'Deleted Invoice.' };
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
 }
